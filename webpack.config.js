58d286bb23cf078efaf3e393392fdd7464295135
hikari <defi@blits.net>
2020-12-04 12:41:12 -0600

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -43,9 +43,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new Dotenv(),
         new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-            }
+            'process.env.NODE_ENV': JSON.stringify('production'),
         })
     ],
     devServer: {
